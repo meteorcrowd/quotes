1e3feb13ba3702ebe9221e50a74c7e0e4ee60350
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-03 11:31:28 +0200

    Added collection and schema.

--- collections.js
+++ collections.js
@@ -1,1 +1,11 @@
 Quotes = new Mongo.Collection("quotes");
+Quotes.attachSchema(new SimpleSchema({
+    quote: {
+        type: String,
+        label: "Quote"
+    },
+    tags: {
+        type: [String],
+        label: "Tags"
+    }
+}));
